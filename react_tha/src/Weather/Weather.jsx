258f26d71228a4df869e111dc852ed51a94987c1
Chaitanya Khachane <ckhachane67@gmail.com>
2021-07-29 22:25:46 +0530

    weather app

--- react_tha/src/Weather/Weather.jsx
+++ react_tha/src/Weather/Weather.jsx
@@ -2,43 +2,95 @@ import React, { useState } from 'react';
 import useFetch from './useFetch';
 
 const Weather = () => {
-  const [city, setCity] = useState('pune');
-  const url = `https://api.weatherapi.com/v1/current.json?key=9ead8543da2d4756aa95345121280&q=${city}`;
+  const [city, setCity] = useState('mumbai');
+  const url = `https://api.weatherapi.com/v1/current.json?key=9ead8543da2d4756aa953451212807&q=${city}`;
 
-  const [apiData, loading, error, fetchData] = useFetch(url);
-
-  // As im changing input it is rerednering evrything
+  const [apiData,  loading, error, fetchData] = useFetch(url);
 
   let handleChange = (e) => {
-    setCity(e.target.value);
+    let newCity = e.target.value;
+    setCity(newCity);
+    // fetchData();
   };
 
   return (
-    <div>
-      <h1>Hello Weather</h1>
-      <input type='text' onChange={handleChange} />
-      <button onClick={() => fetchData()}>Get City</button>
-      <section>
-        {error && <p>{error}</p>}
-        {loading ? <p>Loaidng....</p> : null}
-        {apiData.location && (
-          <div>
-            {/* {JSON.stringify(apiData.location)} */}
-            <h3>{apiData.location.name}</h3>
-            <p>{apiData.location.localtime}</p>
-          </div>
-        )}
+    <section>
+      {/*                 heading               */}
+      <h1
+        style={{
+          marginBottom: '20px',
+          textShadow: '2px 3px 1px #d5d4e9  ',
+        }}>
+        HEmlo Prople who are watching rcorded lectures
+      </h1>
 
-        {apiData.current && (
+      {/*                 form                  */}
+      
+      <form
+        style={{
+          padding: '20px',
+        }}>
+        <input
+          style={{
+            padding: '8px',
+            outline: 'none',
+            marginRight: '9px',
+            width: '200px',
+          }}
+          placeholder='Enter your City name here'
+          type='text'
+          //   value={city}
+          onChange={handleChange}
+        />
+        <button
+          style={{
+            padding: '8px',
+            cursor: 'pointer',
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            fetchData();
+          }}>
+          Get City
+        </button>
+      </form>
+
+      {/*                showing data                         */}
+      <section
+        style={{
+          padding: '20px',
+        }}>
+        {error && <p>{error}</p>}
+        {loading ? (
+          <p>Loading ....</p>
+        ) : (
           <div>
-            <h2>Current Tempearature: {apiData.current.temp_c}</h2>
-            <h2>Condition: {apiData.current.condition.text}</h2>
-            <img src={apiData.current.condition.icon} alt='weather condition' />
-            <h2>Humidity : {apiData.current.humidity}</h2>
+            {apiData.location && (
+              <div
+                style={{
+                  padding: '8px',
+                }}>
+                {/* {JSON.stringify(apiData.location)} */}
+                <h4>{apiData.location.name}</h4>
+                <h4>{apiData.location.localtime}</h4>
+              </div>
+            )}
+            {apiData.current && (
+              <div
+                style={{
+                  padding: '8px',
+                }}>
+                <h2>{apiData.current.condition.text}</h2>
+                <h3>
+                  Currnet Temperature : {apiData.current.temp_c} degrees C.
+                </h3>
+                <img src={apiData.current.condition.icon} alt='' />
+              </div>
+            )}
           </div>
         )}
       </section>
-    </div>
+    </section>
   );
 };
 
